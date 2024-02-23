99a630a410c68960a514e344adab80a45ac2d1fc
slama-pakhrin <salonlama47@gmail.com>
2024-02-22 19:01:45 -0500

    Initial Commit

---
+++ practicefeb22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practicefeb22
+//
+//  Created by Arii on 2024-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
